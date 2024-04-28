9ad2d0517c8ca2413939bc8f5608136136bf1a42
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-28 17:47:03 +0300

    Add new models

---
+++ Models/City.cs
@@ -0,0 +1,8 @@
+﻿namespace HW6_Routing.Models
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace HW6_Routing.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public List<City> Cities { get; set; } = new List<City>();
+    }
+}
