5c80c1b41a59d74db8479bf2303692f03aaaf50b
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-29 21:29:15 +0300

    Remake model

--- Models/City.cs
+++ Models/City.cs
@@ -1,9 +1,18 @@
-﻿namespace HW6_Routing.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HW6_Routing.Models
 {
     public class City
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public int Population { get; set; }
+        
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+
     }
 }
--- Models/Country.cs
+++ Models/Country.cs
@@ -1,10 +1,15 @@
-﻿namespace HW6_Routing.Models
+﻿using Microsoft.Extensions.Hosting;
+using System.Numerics;
+
+namespace HW6_Routing.Models
 {
     public class Country
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Population { get; set; }
-        public List<City> Cities { get; set; } = new List<City>();
+
+        public ICollection<City> Cities { get; set; } = new List<City>();
+
     }
 }
